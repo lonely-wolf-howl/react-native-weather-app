b7bf3144dd40351a5abbd6d5605807f73c0d1c1f
kme-developer <electruc0095@gmail.com>
2025-03-27 23:03:40 +0900

    add. get device location.

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
 
 const { width: screenWidth } = Dimensions.get('window');
 
 export default function App() {
+  const [location, setLocation] = useState('loading');
+  const [city, setCity] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMessage('permission to access location was denied');
+        return;
+      }
+
+      const { coords } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+      const location = await Location.reverseGeocodeAsync(coords);
+
+      setLocation(location[0]);
+      setCity(location[0].city);
+    }
+
+    getCurrentLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView pagingEnabled={true} horizontal={true} showsHorizontalScrollIndicator={false}>
         <View style={styles.day}>
@@ -33,8 +58,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cityName: {
-    fontSize: 60,
-    fontWeight: '500',
+    fontSize: 45,
+    fontWeight: '600',
   },
   day: {
     width: screenWidth,
@@ -43,7 +68,7 @@ const styles = StyleSheet.create({
   temperature: {
     marginTop: 50,
     fontSize: 175,
-    fontWeight: '500',
+    fontWeight: '400',
   },
   description: {
     marginTop: -30,
